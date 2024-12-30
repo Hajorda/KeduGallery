53da1e1a4faeba674b836bd93c81963be37b4645
Hajorda <hajorda@gmail.com>
2024-12-30 21:37:34 +0300

    fix config

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,19 @@
-export default {
-    base: '/KeduGallery/',
-  };
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/KeduGallery/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    chunkSizeWarningLimit: 500, // Adjust the chunk size warning limit if needed
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Split vendor code into a separate chunk
+          vendor: ['three'],
+        },
+      },
+    },
+  },
+  assetsInclude: ['**/*.gltf', '**/*.glb', '**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.svg'],
+});
